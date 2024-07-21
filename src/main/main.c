f31027e40d96ead5b92a4d4b1472568b56031e21
tas <122511599+tasmanday@users.noreply.github.com>
2024-07-21 14:53:25 +0930

    working on parsing

--- src/main/main.c
+++ src/main/main.c
@@ -6,25 +6,25 @@
 /*   By: tday <tday@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:17:07 by tday              #+#    #+#             */
-/*   Updated: 2024/07/20 00:03:59 by tday             ###   ########.fr       */
+/*   Updated: 2024/07/21 14:52:53 by tday             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
 
-int main(void)
+int	main(void)
 {
-    t_data data;
+	t_data	data;
 
 	init_window(&data);
-    data.mlx = mlx_init();
-    data.win = mlx_new_window(data.mlx, data.width, data.height, "mini_rt");
-    init_img(&data);
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, data.width, data.height, "mini_rt");
+	init_img(&data);
 
-    mlx_hook(data.win, ON_DESTROY, 0, close_window, &data);
-    mlx_key_hook(data.win, key_hook, &data);
-    mlx_loop_hook(data.mlx, loop_function, &data);
+	mlx_hook(data.win, ON_DESTROY, 0, close_window, &data);
+	mlx_key_hook(data.win, key_hook, &data);
+	mlx_loop_hook(data.mlx, loop_function, &data);
 
-    mlx_loop(data.mlx);
-    return (0);
+	mlx_loop(data.mlx);
+	return (0);
 }
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,36 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   frame.c                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tday <tday@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/19 20:28:38 by tday              #+#    #+#             */
-/*   Updated: 2024/07/21 14:52:54 by tday             ###   ########.fr       */
+/*   Created: 2024/07/21 13:32:44 by tday              #+#    #+#             */
+/*   Updated: 2024/07/21 14:52:55 by tday             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minirt.h"
 
-void	swap_images(t_data *data)
-{
-	int		temp;
-
-	temp = data->draw_img;
-	data->draw_img = data->display_img;
-	data->display_img = temp;
-}
-
-int	update_frame(t_data *data)
-{
-	mlx_put_image_to_window(data->mlx, data->win, \
-		data->img[data->display_img].img, 0, 0);
-	swap_images(data);
-	return (0);
-}
-
-int	loop_function(t_data *data)
-{
-	update_frame(data);
-	return (0);
-}
