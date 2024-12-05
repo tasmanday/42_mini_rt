34a8bec1f25ea8d7ef71ec29e7bd22c111db36ee
tas <122511599+tasmanday@users.noreply.github.com>
2024-12-05 15:13:16 +1030

    working on tracing rays

---
+++ src/ray_casting/trace_rays.c
@@ -0,0 +1,59 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trace_rays.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tday <tday@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 14:47:47 by tday              #+#    #+#             */
+/*   Updated: 2024/12/05 15:13:05 by tday             ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "../../inc/minirt.h"
+
+void	init_pixel_array(t_mem *mem, t_Scene scene)
+{
+	int		y;
+	int		x;
+
+	// put in init_corner_rays() function
+	y = 0;
+	while (y <= scene.mlx.height)
+	{
+		x = 0;
+		while (x <= scene.mlx.width)
+		{
+			init_ray(&scene, &mem->corners[y][x], x, y);
+			x++;
+		}
+		y++;
+	}
+
+	// put in init_pixels() function
+	y = 0;
+	while (y< scene.mlx.height)
+	{
+		x = 0;
+		while (x < scene.mlx.width)
+		{
+			mem->pixels[y][x].TL = &mem->corners[y][x]; // top left corner
+			mem->pixels[y][x].TR = &mem->corners[y][x + 1]; // top right corner
+			mem->pixels[y][x].BL = &mem->corners[y + 1][x]; // bottom left corner
+			mem->pixels[y][x].BR = &mem->corners[y + 1][x + 1]; // bottom right corner
+
+			init_ray(&scene, &mem->pixels[y][x].mid, x + 0.5, y + 0.5);
+
+			mem->pixels[y][x].avg_colour.r = 0;
+			mem->pixels[y][x].avg_colour.g = 0;
+			mem->pixels[y][x].avg_colour.b = 0;
+		}
+	}
+}
+
+
+void	trace_rays(t_mem *mem, t_Scene scene)
+{
+	init_pixel_array(mem, scene);
+
+}
